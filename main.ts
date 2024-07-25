4cad0d565ca00447ced84ebfe13eb8b698f7beb1
Brianna Chea <briannachea2008@icloud.com>
2024-07-25 13:34:16 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 enum RadioMessage {
     message1 = 49434,
     manual_water = 45559,
-    auto_water = 51825
+    auto_water = 51825,
+    water_success = 40104
 }
 radio.onReceivedMessage(RadioMessage.manual_water, function () {
     basic.showLeds(`
@@ -11,5 +12,10 @@ radio.onReceivedMessage(RadioMessage.manual_water, function () {
         . # # # .
         . . # . .
         `)
+    basic.pause(5000)
+    radio.sendMessage(RadioMessage.water_success)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Duck)
 })
 radio.setGroup(99)
+basic.showIcon(IconNames.Duck)
