67caf1fac606e4b8cba9511207ad99ff405d1fa0
angel-cak3s <123357569+angel-cak3s@users.noreply.github.com>
2024-07-25 13:29:26 -0700

    Receive radio, show led

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+enum RadioMessage {
+    message1 = 49434,
+    manual_water = 45559,
+    auto_water = 51825
+}
+radio.onReceivedMessage(RadioMessage.manual_water, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
 })
+radio.setGroup(99)
